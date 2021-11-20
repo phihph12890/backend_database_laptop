a308bfff265dde24b8ff2a0aafbea2a142ff1abd
phihph12890 <65588792+phihph12890@users.noreply.github.com>
2021-11-20 10:04:50 +0700

    upload project

---
+++ models/product.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({
+    category: {
+        type: ObjectId,
+        ref: "Category", 
+        required: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        
+    },
+    priceSale: {
+        type: Number,
+        required: true,
+        
+    },
+    guarantee: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    config: {
+        type: String,
+        required: true,
+        
+    },
+    description: {
+        type: String,
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    shipping: {
+        required: false,
+        type: Boolean
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model("Product", productSchema);
